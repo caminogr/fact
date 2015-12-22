0eef99f73f19df3d2dc4082831a3cef1c47c6261
上之浦孝樹 <amino.t02y@gmail.com>
2015-12-22 23:23:49 +0900

    Execute rake db:migrate command

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151220155105) do
+ActiveRecord::Schema.define(version: 20151222141256) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20151220155105) do
     t.string   "job",                    limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.string   "avatar",                 limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
