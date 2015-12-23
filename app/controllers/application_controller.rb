1b1163a8792c6e3b87cdab307c067e9f8f0500cd
上之浦孝樹 <amino.t02y@gmail.com>
2015-12-23 20:34:38 +0900

    Add avatar to strong parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
 
 private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up).push(:family_name, :first_name, :gender, :age, :university, :job)
+    devise_parameter_sanitizer.for(:sign_up).push(:family_name, :first_name, :gender, :age, :university, :job, :avatar)
   end
 end
