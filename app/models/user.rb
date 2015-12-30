eb4fb4147997f45ff45a5f6cba666c4a3dce5747
上之浦孝樹 <amino.t02y@gmail.com>
2015-12-30 23:45:16 +0900

    Set association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :events_users
+  has_many :events, through: :events_users
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
