1877f537056a06a4e5568ff2f31f09787912f573
上之浦孝樹 <amino.t02y@gmail.com>
2016-01-06 23:28:39 +0900

    Set scope

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,8 @@ class Event < ActiveRecord::Base
   has_many :events_users
   has_many :users, through: :events_users
 
+  scope :owner, ->(id){ Event.find(id).users.first }
+
   validates :title,
             :sport,
             :fixed_number,
