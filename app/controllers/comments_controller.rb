58768b2e4bfd5fa589d2c663fe61cffaf855842c
上之浦孝樹 <amino.t02y@gmail.com>
2016-01-09 22:29:23 +0900

    Set create action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,10 @@
 class CommentsController < ApplicationController
+  def create
+    current_user.comments.create(comment_params)
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(event_id: params[:event_id])
+  end
 end
