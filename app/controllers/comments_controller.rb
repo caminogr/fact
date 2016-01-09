2116dc66f9c40ea39832865c9d327a8d661f28d6
上之浦孝樹 <amino.t02y@gmail.com>
2016-01-10 01:48:04 +0900

    Create destroy action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,6 +4,10 @@ class CommentsController < ApplicationController
     redirect_to event_path(params[:event_id])
   end
 
+  def destroy
+    Comment.find(params[:id]).destroy
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:text).merge(event_id: params[:event_id])
