3bd6503b5cc914360d17b896fe08be1fce488181
上之浦孝樹 <amino.t02y@gmail.com>
2016-01-10 14:27:00 +0900

    Add redirect_to method

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,6 +2,7 @@ class FavoritesController < ApplicationController
 
   def create
     current_user.favorites.create(event_id: params[:event_id])
+    redirect_to event_path(params[:event_id])
   end
 
 end
