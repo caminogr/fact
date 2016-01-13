4bcdc81c09442cfb815139f8531ccab1c5430983
上之浦孝樹 <amino.t02y@gmail.com>
2016-01-13 17:28:19 +0900

    Add favorite_event action to get events current_user favorite

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  def favorite_events
+    @events = current_user.favorite_events
+    render :show
+  end
+
   def show
     @events = current_user.events
   end
