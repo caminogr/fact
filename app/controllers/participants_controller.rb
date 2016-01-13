5436e5f08ed015751a7f2ae7c283d943d1098e75
上之浦孝樹 <amino.t02y@gmail.com>
2016-01-13 20:11:04 +0900

    Add redirect_to method

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -1,5 +1,6 @@
 class ParticipantsController < ApplicationController
   def create
     current_user.participants.create(event_id: params[:event_id])
+    redirect_to event_path(params[:event_id])
   end
 end
