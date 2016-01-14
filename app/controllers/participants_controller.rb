38c621d01ffddf296761c6f3cbb081b793468a9f
上之浦孝樹 <amino.t02y@gmail.com>
2016-01-14 12:07:11 +0900

    Add method not to act with sign_outed

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -1,4 +1,7 @@
 class ParticipantsController < ApplicationController
+
+  before_action :authenticate_user!, only: [:create, :destroy]
+
   def create
     current_user.participants.create(event_id: params[:event_id])
     redirect_to event_path(params[:event_id])
