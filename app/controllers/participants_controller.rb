721d90d039099c05e962f13fc94b2c16fd247fac
上之浦孝樹 <amino.t02y@gmail.com>
2016-01-14 12:03:49 +0900

    Add destroy action to cease to participate the event

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -3,4 +3,8 @@ class ParticipantsController < ApplicationController
     current_user.participants.create(event_id: params[:event_id])
     redirect_to event_path(params[:event_id])
   end
+
+  def destroy
+    @paricipant = Participant.find(params[:id]).destroy
+  end
 end
