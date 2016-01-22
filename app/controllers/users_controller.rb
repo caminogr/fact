cc62616fe0374acb1c56b23ba4db6c8859ace1b1
上之浦孝樹 <amino.t02y@gmail.com>
2016-01-22 13:41:14 +0900

    Add set_event method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,18 @@
 class UsersController < ApplicationController
+
+  before_action :set_event, only: [:favorite_events, :show]
+
   def favorite_events
-    @user = User.find(params[:id])
     @events = @user.favorite_events
     render :show
   end
 
   def show
-    @user = User.find(params[:id])
     @events = @user.events
   end
+
+  private
+    def set_event
+      @user = User.find(params[:id])
+    end
 end
