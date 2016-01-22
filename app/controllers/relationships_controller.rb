e6a2a5a076ac07ec768e6ba6ddd6cbce22b54d1c
上之浦孝樹 <amino.t02y@gmail.com>
2016-01-22 15:16:00 +0900

    Add redirect_to method

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,5 +1,6 @@
 class RelationshipsController < ApplicationController
   def create
     current_user.active_relationships.create(following_id: params[:user_id])
+    redirect_to user_path(params[:user_id])
   end
 end
