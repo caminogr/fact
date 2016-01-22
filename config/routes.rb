39683a563285f5c5142754c2e68f2e2ae360ee3a
上之浦孝樹 <amino.t02y@gmail.com>
2016-01-22 13:57:15 +0900

    Nest resources :relationships inside :users

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   end
   devise_for :users
   resources :users, only: [:show] do
+    resources :relationships, only: [:index, :create, :destroy]
     member do
       get :favorite_events
     end
