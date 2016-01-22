e5474e7be43adacef4d7ab3cdb10b47d8161e131
上之浦孝樹 <amino.t02y@gmail.com>
2016-01-22 18:29:06 +0900

    Add destroy action at users controller

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,4 +3,9 @@ class RelationshipsController < ApplicationController
     current_user.active_relationships.create(following_id: params[:user_id])
     redirect_to user_path(params[:user_id])
   end
+
+  def destroy
+    Relationship.find(params[:id]).destroy
+    redirect_to user_path(params[:user_id])
+  end
 end
