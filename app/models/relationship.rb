405ff3763d7d85f23480d8deb0e16d4b1fc3d8bb
上之浦孝樹 <amino.t02y@gmail.com>
2016-01-23 12:31:50 +0900

    Add validation not to allow to follow yourself

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,3 +1,11 @@
 class Relationship < ActiveRecord::Base
   belongs_to :user
+
+  validate :follow_yourself?
+
+  def follow_yourself?
+    if user_id == following_id
+       errors.add(:following, "cant`t follow yourself")
+    end
+  end
 end
