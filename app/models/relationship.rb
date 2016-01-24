8a5dde767bcbbfd3251096e8f970737b7199d3c8
上之浦孝樹 <amino.t02y@gmail.com>
2016-01-24 12:22:52 +0900

    Fix description at Relationship model file

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,6 +1,6 @@
 class Relationship < ActiveRecord::Base
-  belongs_to :user, :follower, class_name: "User"
-  belongs_to :user, :followed, class_name: "User"
+  belongs_to :follower, class_name: "User"
+  belongs_to :followed, class_name: "User"
 
   validate :follow_yourself?
 
