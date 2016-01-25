a3be8e9d28b0822b5bbed96df12b7fb0693ef8f7
上之浦孝樹 <amino.t02y@gmail.com>
2016-01-26 01:48:21 +0900

    Add association between inviter and invited_user model through middle table, invitations table

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,2 +1,4 @@
 class Invitation < ActiveRecord::Base
+  belongs_to :inviter, class_name: "User"
+  belongs_to :invited, class_name: "User"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,16 @@ class User < ActiveRecord::Base
   has_many :comments
   has_many :favorites
   has_many :favorite_events, through: :favorites, source: :event
+
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :delete_all
   has_many :following_users, through: :active_relationships, source: :followed
   has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :delete_all
   has_many :followers, through: :passive_relationships, source: :follower
+
+  has_many :active_invitationships, class_name: "Invitation", foreign_key: "inviter_id", dependent: :delete_all
+  has_many :invited_users, through: :active_invitationships, source: :invited
+  has_many :passive_invitationships, class_name: "Invitationship", foreign_key: "invited_id", dependent: :delete_all
+  has_many :inviters, through: :passive_invitationships, source: :inviter
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
