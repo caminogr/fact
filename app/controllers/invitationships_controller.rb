cee33e67d1e52be46d82cbc4597707810ad899a0
上之浦孝樹 <amino.t02y@gmail.com>
2016-01-27 02:47:32 +0900

    Add create action at invitationships controller

--- app/controllers/invitationships_controller.rb
+++ app/controllers/invitationships_controller.rb
@@ -3,4 +3,9 @@ class InvitationshipsController < ApplicationController
     @event = Event.find(params[:event_id])
     @invitable_users = @event.owner.followers
   end
+
+  def create
+    current_user.active_invitationships.create(invited_id: params[:invited_id], event_id: params[:event_id])
+    redirect_to action: :invitable
+  end
 end
