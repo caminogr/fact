8ae5cd36db77f9763d3885a40514aa8669ded53d
上之浦孝樹 <amino.t02y@gmail.com>
2016-01-27 03:01:19 +0900

    Add invited action to invitationships controller

--- app/controllers/invitationships_controller.rb
+++ app/controllers/invitationships_controller.rb
@@ -4,6 +4,11 @@ class InvitationshipsController < ApplicationController
     @invitable_users = @event.owner.followers
   end
 
+  def invited
+    @user = User.find(params[:user_id])
+    @invitationships = @user.passive_invitationships
+  end
+
   def create
     current_user.active_invitationships.create(invited_id: params[:invited_id], event_id: params[:event_id])
     redirect_to action: :invitable
