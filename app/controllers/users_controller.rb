6f1d5c70af35348e50b49c496a73e0551cb2f2ef
上之浦孝樹 <amino.t02y@gmail.com>
2016-01-29 00:08:51 +0900

    Remove needless method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,6 @@
 class UsersController < ApplicationController
 
-  before_action :set_event, only: :show
-
   def show
     @events = @user.events
     @following = current_user.active_relationships.find_by(followed_id: @user) if user_signed_in?
   end
-
-  private
-    def set_event
-      @user = User.find(params[:id])
-    end
-end
