ad892f588fd08d2b5a2e71f03ba12f6e983ef0b4
上之浦孝樹 <amino.t02y@gmail.com>
2016-01-29 01:03:23 +0900

    Modify description

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
 
   def show
+    @user = User.find(params[:id])
     @events = @user.events
     @following = current_user.active_relationships.find_by(followed_id: @user) if user_signed_in?
   end
+end
