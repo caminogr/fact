6578f2df2527c9c54ff73eb272f8e662abbd9495
上之浦孝樹 <amino.t02y@gmail.com>
2016-02-03 02:23:51 +0900

    Add search action at groups controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,4 +2,8 @@ class GroupsController < ApplicationController
   def new
     @group = Group.new
   end
+
+  def search
+    @users = User.where('family_name LIKE(?)', "%#{params[:keyword]}%")
+  end
 end
