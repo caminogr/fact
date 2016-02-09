74a5291205001ee44c0576fc0cb18e6c5bdb35e0
上之浦孝樹 <amino.t02y@gmail.com>
2016-02-09 17:12:30 +0900

    Hand off user_id from component user to component user_list when click user`s name

--- app/assets/javascripts/components/user.js.jsx
+++ app/assets/javascripts/components/user.js.jsx
@@ -1,9 +1,15 @@
 var User = React.createClass({
 
+  handleClickUser: function(){
+    var user_id = this.props.user_id
+    this.props.onClickUser(user_id);
+  },
+
+
   render: function(){
     return(
       <div className="User">
-        <p>
+        <p onClick={this.handleClickUser}>
           {this.props.name}
         </p>
       </div>
--- app/assets/javascripts/components/user_list.js.jsx
+++ app/assets/javascripts/components/user_list.js.jsx
@@ -1,9 +1,11 @@
 var UserList = React.createClass({
 
+  handleClickUser: function(){
+  },
 
   render: function(){
     return(
-      <li className="UserList">
+      <li className="UserList" onClick={this.handleClickUser}>
         <User className="User" name={this.props.name}/>
       </li>
     );
