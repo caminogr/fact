c7e42087cb1c5db59523153f75b77c79b7e76cf9
上之浦孝樹 <amino.t02y@gmail.com>
2016-02-10 02:00:19 +0900

    Remove needless function

--- app/assets/javascripts/components/user.js.jsx
+++ app/assets/javascripts/components/user.js.jsx
@@ -1,15 +1,9 @@
 var User = React.createClass({
 
-  handleClickUser: function(){
-    var user_id = this.props.user_id
-    this.props.onClickUser(user_id);
-  },
-
-
   render: function(){
     return(
       <div className="User">
-        <p onClick={this.handleClickUser}>
+        <p>
           {this.props.name}
         </p>
       </div>
