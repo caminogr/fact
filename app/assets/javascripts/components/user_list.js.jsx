dc2e3a7f5ee42e7b925b75cb6207764ba0d7f3ed
上之浦孝樹 <amino.t02y@gmail.com>
2016-02-09 17:17:44 +0900

    Hand off id and name of user clicked

--- app/assets/javascripts/components/user_list.js.jsx
+++ app/assets/javascripts/components/user_list.js.jsx
@@ -1,12 +1,13 @@
 var UserList = React.createClass({
 
   handleClickUser: function(){
+    this.props.onClickUser(this.props.user_id, this.props.name);
   },
 
   render: function(){
     return(
       <li className="UserList" onClick={this.handleClickUser}>
-        <User className="User" name={this.props.name}/>
+        <User className="User" name={this.props.name} user_id={this.props.user_id} />
       </li>
     );
   }
--- app/assets/javascripts/components/user_search_box.js.jsx
+++ app/assets/javascripts/components/user_search_box.js.jsx
@@ -8,10 +8,15 @@ var UserSearchBox = React.createClass({
     };
   },
 
+  handleClick: function(userId, userName) {
+
+  },
+
+
   render: function(){
     var userlist = this.state.filteredUser.map(function(user){
 
-    return (<UserList key={user.id} user_id={user.id} name={user.first_name}/>);
-    });
+    return (<UserList key={user.id} user_id={user.id} name={user.first_name} onClickUser={this.handleClick}/>);
+    }.bind(this));
   }
 });
