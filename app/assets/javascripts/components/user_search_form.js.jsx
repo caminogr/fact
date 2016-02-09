847f122dae26503c212399ca8540338caea48df3
上之浦孝樹 <amino.t02y@gmail.com>
2016-02-09 22:14:49 +0900

    add hidden type input tag  with name and value property

--- app/assets/javascripts/components/user_search_form.js.jsx
+++ app/assets/javascripts/components/user_search_form.js.jsx
@@ -6,9 +6,14 @@ var UserSearchForm = React.createClass({
 
   render: function(){
 
+    var hiddenlist = this.props.selectedUserId.map(function(userId){
+      return(<input key={userId} type="hidden" name="user_id" value={userId} />);
+    });
+
     return(
       <form className="UserSearchForm"  >
         <input type="text" ref="username" onKeyUp={this.handleWriteDown}/>
+        {hiddenlist}
       </form>
     );
   }
