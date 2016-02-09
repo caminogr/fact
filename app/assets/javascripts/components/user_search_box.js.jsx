b4fc9465901f74964eb96a02ee18c023d7fbce0d
上之浦孝樹 <amino.t02y@gmail.com>
2016-02-09 14:59:58 +0900

    Set state at use_search_box component

--- app/assets/javascripts/components/user_search_box.js.jsx
+++ app/assets/javascripts/components/user_search_box.js.jsx
@@ -1,6 +1,12 @@
 var UserSearchBox = React.createClass({
 
-
+  getInitialState: function(props){
+    return{
+      filteredUser: this.props.users,
+      selectedUserId: [],
+      selectedUserName: []
+    };
+  },
 
   render: function(){
 
