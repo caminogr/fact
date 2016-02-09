4f1836994a840dd25ca2d3f178eb13076b6f7782
上之浦孝樹 <amino.t02y@gmail.com>
2016-02-09 15:25:22 +0900

    Hand off user.id and user.first_name from user_search_box to list user_list

--- app/assets/javascripts/components/user_search_box.js.jsx
+++ app/assets/javascripts/components/user_search_box.js.jsx
@@ -11,7 +11,7 @@ var UserSearchBox = React.createClass({
   render: function(){
     var userlist = this.state.filteredUser.map(function(user){
 
-    return (<UserList/>);
-    };
+    return (<UserList key={user.id} user_id={user.id} name={user.first_name}/>);
+    });
   }
 });
