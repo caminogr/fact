4baecdc1ddea888b96aa1608251a4243cb717794
上之浦孝樹 <amino.t02y@gmail.com>
2016-02-11 09:11:42 +0900

    Add create action to groups_controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,7 +4,17 @@ class GroupsController < ApplicationController
     @users = User.all
   end
 
+  def create
+    @group = Group.new(group_params)
+    render :new unless @group.save
+  end
+
   def search
     @users = User.where('family_name LIKE(?)', "%#{params[:keyword]}%")
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name, user_ids: [])
   end
 end
