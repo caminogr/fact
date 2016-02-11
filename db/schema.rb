df2aae2e1197007cb22914a5e771f12d6a3be0ea
上之浦孝樹 <amino.t02y@gmail.com>
2016-02-12 01:52:29 +0900

    Add group_id column to events table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160202123005) do
+ActiveRecord::Schema.define(version: 20160211164941) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "text",       limit: 65535
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20160202123005) do
     t.integer  "favorites_count", limit: 4,     default: 0
     t.datetime "deadline"
     t.string   "fee",             limit: 255
+    t.integer  "group_id",        limit: 4,     default: 0
   end
 
   add_index "events", ["owner_id"], name: "index_events_on_owner_id", using: :btree
