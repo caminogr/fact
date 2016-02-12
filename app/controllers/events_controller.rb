da939e7792202932270e4986bcd780bf0f12d846
上之浦孝樹 <amino.t02y@gmail.com>
2016-02-12 22:36:29 +0900

    Use open instead of all

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,7 +4,7 @@ class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
 
   def index
-    @events = Event.all.newer
+    @events = Event.open.newer
   end
 
   def show
