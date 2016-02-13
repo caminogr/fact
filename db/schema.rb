2de9b800567d32c48742fe3820b9fa3c5afa78a4
上之浦孝樹 <amino.t02y@gmail.com>
2016-02-14 01:16:38 +0900

    Create prefectures table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160211164941) do
+ActiveRecord::Schema.define(version: 20160213142552) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "text",       limit: 65535
@@ -77,6 +77,11 @@ ActiveRecord::Schema.define(version: 20160211164941) do
     t.integer  "user_id",    limit: 4
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "prefectures", force: :cascade do |t|
+    t.string "name",      limit: 255
+    t.string "name_kana", limit: 255
   end
 
   create_table "relationships", force: :cascade do |t|
