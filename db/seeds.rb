efe9634b6fb0f16b342707a02f8b3f765a2c9f72
上之浦孝樹 <amino.t02y@gmail.com>
2016-02-15 02:26:30 +0900

    Add prefecture_id to prefectures.csv file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 require "csv"
 
 CSV.foreach('db/prefectures.csv') do |row|
-  Prefecture.create(:name => row[0], :name_kana => row[1])
+  Prefecture.create(:prefecture_id => row[0], :name => row[1], :name_kana => row[2])
 end
 
 CSV.foreach('db/cities.csv') do |row|
