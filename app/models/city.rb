4147f7fab835ea791a0f685313367b0e32077b32
上之浦孝樹 <amino.t02y@gmail.com>
2016-02-16 17:46:28 +0900

    Add association among user, prefecture and city model

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,3 +1,5 @@
 class City < ActiveRecord::Base
   belongs_to :prefecture
+
+  has_many :events
 end
--- app/models/prefecture.rb
+++ app/models/prefecture.rb
@@ -3,4 +3,5 @@ class Prefecture < ActiveRecord::Base
   self.primary_key = :prefecture_id
 
   has_many :cities
+  has_many :events
 end
