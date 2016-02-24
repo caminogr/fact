8e3a417bbd248950fda7652f434550fad8552704
上之浦孝樹 <amino.t02y@gmail.com>
2016-02-24 18:34:37 +0900

    Add img tag

--- app/assets/javascripts/components/user.js.jsx
+++ app/assets/javascripts/components/user.js.jsx
@@ -3,6 +3,7 @@ var User = React.createClass({
   render: function(){
     return(
       <div className="User">
+        <img src={this.props.user.avatar.url} className="border-radius" />
         <a>
           {this.props.user.first_name}
         </a>
