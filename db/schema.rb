bb799c57884704b796840e2e09b5c99c24b7e100
上之浦孝樹 <amino.t02y@gmail.com>
2016-03-03 02:16:16 +0900

    Add field to save accont_id

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160214011239) do
+ActiveRecord::Schema.define(version: 20160302163846) do
 
   create_table "cities", force: :cascade do |t|
     t.integer "prefecture_id", limit: 4
@@ -149,8 +149,10 @@ ActiveRecord::Schema.define(version: 20160214011239) do
     t.datetime "created_at",                                        null: false
     t.datetime "updated_at",                                        null: false
     t.string   "avatar",                 limit: 255
+    t.string   "account_id",             limit: 255
   end
 
+  add_index "users", ["account_id"], name: "index_users_on_account_id", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
